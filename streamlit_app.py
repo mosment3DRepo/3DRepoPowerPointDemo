0c75631d73e77b42b084644907634fe954e0aa39
mosment3DRepo <62879582+mosment3DRepo@users.noreply.github.com>
2022-09-14 16:35:35 +0100

    added 3D Repo cookies
    Make initial request to 3D Repo to see if cookies are available, if not use API key field

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,21 +4,33 @@ import requests
 import json
 import os
 from io import BytesIO
+from streamlit_cookies_manager import EncryptedCookieManager
 
+st.write("v1.07")
 
-st.header("3D Repo Safetibase PowerPoint App")
-st.text("Insert your Model/Federation details below to generate a Powerpoint file of all the Risks")
+global cookies
 
-teamspace = st.text_input("Teamspace:")
-model = st.text_input("Model:")
-apiKey = st.text_input("API Key:")
-domain = st.text_input("Domain:", value="https://www.3drepo.io")
-output = st.text_input("Output File Name:", value = "3D Repo Safetibase Export")
+cookies = EncryptedCookieManager(
+    # You should really setup a long COOKIES_PASSWORD secret if you're running on Streamlit Cloud.
+    password=os.environ.get("COOKIES_PASSWORD", "My secret password"),
+)
 
 def get_3dreporisks(domain,teamspace,model,api_key):
-    risk_response = requests.get(domain + "/api/"+teamspace+"/"+model+"/risks?key="+api_key)
+    url = domain + "/api/"+teamspace+"/"+model+"/risks?key="+api_key
+    headers = {
+    'Cookie': 'connect.sid=' + cookies['connect.sid']
+    }
+    risk_response = requests.get(url, headers=headers)
     risk_response_object = json.loads(risk_response.text)
     return risk_response_object
+
+def get_3drepologin(domain,teamspace,model):
+    url = domain + "/api/me"
+    headers = {
+    'Cookie': 'connect.sid=' + cookies['connect.sid']
+    }
+    login_response = requests.get(url, headers=headers)
+    return login_response.status_code
 
 def insert(domain,teamspace,model,apiKey,output):
 
@@ -66,6 +78,29 @@ def insert(domain,teamspace,model,apiKey,output):
         file_name=fileName
     )
 
+if not cookies.ready():
+    # Wait for the component to load and send us current cookies.
+    st.stop()
+
+st.write("Current cookies:", cookies['connect.sid'])
+
+login_response = get_3drepologin
+login_response_success = login_response == 200
+
+
+st.header("3D Repo Safetibase PowerPoint App")
+st.text("Insert your Model/Federation details below to generate a Powerpoint file of all the Risks")
+
+teamspace = st.text_input("Teamspace:")
+model = st.text_input("Model:")
+
+if not login_response_success:
+    apiKey = st.text_input("API Key:")
+else:
+    apiKey = ''
+
+domain = st.text_input("Domain:", value="https://www.3drepo.io")
+output = st.text_input("Output File Name:", value = "3D Repo Safetibase Export")
 
 if st.button("Submit"):
     insert(domain,teamspace,model,apiKey,output)
